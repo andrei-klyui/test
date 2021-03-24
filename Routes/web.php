63bb9ceec64e0d42c752124e83a894638cf2c5c2
andrei-klyui <62928832+andrei-klyui@users.noreply.github.com>
2021-03-24 20:10:26 +0200

    Update web.php

--- Routes/web.php
+++ Routes/web.php
@@ -24,6 +24,34 @@ Route::group(['middleware' => ['auth']], function () {
 
 });
 
+Route::prefix('order')->name('order.')->group(function () {
+        Route::get('/', 'OrderController@index')->name('index');
+        Route::post('/', 'OrderController@store')->name('store');
+        Route::prefix('{id}')->group(function () {
+            Route::get('/', 'OrderController@show')->name('show');
+            Route::put('/', 'OrderController@update')->name('update');
+            Route::get('edit', 'OrderController@edit')->name('edit');
+        });
+
+        Route::get('/new', 'OrderController@newOrders')->name('new');
+        Route::get('/ajax', 'OrderController@getUpdatedOrderTable')->name('ajax-table');
+});
+
+Route::prefix('orders')->name('orders.')->group(function () {
+    Route::post('status/{status}', 'OrderController@changeStatus')->name('status');
+    Route::post('merge', 'OrderController@merge')->name('merge');
+});
+
+Route::prefix('item')->name('item.')->group(function () {
+    Route::post('/', 'ItemController@store')->name('store');
+
+    Route::prefix('{id}')->group(function () {
+        Route::delete('/', 'ItemController@destroy')->name('destroy');
+        Route::put('/', 'ItemController@update')->name('update');
+        Route::get('service', 'ItemController@service')->name('service');
+    });
+});
+
 Route::post('/login', function () {
     return view('login');
 })->name('login');
